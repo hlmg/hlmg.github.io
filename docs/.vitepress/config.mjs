9bc74fc9afe8c325048305d7aa3e28ecc3c8aad4
hlmg <lmgzxcv@gmail.com>
2023-09-22 18:30:48 +0900

    사이트 생성
    Vitepress: https://github.com/vuejs/vitepress
    
    vitepress-sidebar: https://github.com/jooy2/vitepress-sidebar

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+import { generateSidebar } from 'vitepress-sidebar';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "StudyLog",
+  description: "A Site Description",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+    sidebar: getSidebar(),
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
+
+// https://github.com/jooy2/vitepress-sidebar#options
+function getSidebar() {
+  return generateSidebar({
+    documentRootPath: '/docs',
+    useTitleFromFileHeading: true,
+    collapsed: true,
+  })
+}
